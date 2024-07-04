545d486dafd291fd83f7edb3b1539c6d45ee5dda
Akshat Sharma <webdevakshat@gmail.com>
2024-07-04 18:00:11 +0530

    Fetch : Coins Data on first render and on pagination.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { useEffect, useState } from "react";
+import { fetchCoinsData } from "./functions/getData";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [pagination, setPagination] = useState(1);
+
+  useEffect(() => {
+    fetchCoinsData(10, pagination).then((value) => {
+      setData(value);
+    });
+  }, [pagination]);
+
   return (
     <>
       <Navbar />
---
+++ src/functions/getData.js
@@ -0,0 +1,14 @@
+export const fetchCoinsData = async (perpage , page) => {
+    const api = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${perpage}&page=${page}&sparkline=false`
+    try {
+        const response = await fetch(api);
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Error getting response.");
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+  }
