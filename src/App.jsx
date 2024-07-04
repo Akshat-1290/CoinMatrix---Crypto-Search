68fd43139159733084fe0b9f7c3bcd7510bbd01c
Akshat Sharma <webdevakshat@gmail.com>
2024-07-04 18:37:28 +0530

    Use Context Configured to share fetched across multiple components.

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,32 @@ import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { useEffect, useState } from "react";
 import { fetchCoinsData } from "./functions/getData";
-
+import { CoinsContext } from "./context/AppContext";
 function App() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [pagination, setPagination] = useState(1);
 
   useEffect(() => {
+    console.log("fething");
+    setLoading(true);
     fetchCoinsData(10, pagination).then((value) => {
       setData(value);
+      setLoading(false);
     });
   }, [pagination]);
+
+  const updatePagination = () => {
+    setPagination(pagination + 1);
+  };
 
   return (
     <>
       <Navbar />
       <main>
-        <Outlet />
+        <CoinsContext.Provider value={{ data, updatePagination, loading }}>
+          <Outlet />
+        </CoinsContext.Provider>
       </main>
     </>
   );
