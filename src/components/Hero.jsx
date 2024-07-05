16e8f7f97d4c29ded95ce386e52e166f05e58379
Akshat Sharma <webdevakshat@gmail.com>
2024-07-05 16:24:40 +0530

    Displaying Limited Coins in hero section by accessing the data from context

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
           <button className="scroll-to-market bg-gradient-purple w-2/3 h-14 rounded-full text-xl flex justify-center items-center gap-3 sm:hidden">
             See Prices <i className="fa-solid fa-angle-down"></i>
           </button>
-        <HeroCoins/>
+          <HeroCoins />
         </div>
       </section>
     </>
--- src/components/HeroCoins.jsx
+++ src/components/HeroCoins.jsx
@@ -1,67 +1,36 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CoinsContext } from "../context/AppContext";
 
 export const HeroCoins = () => {
+  const { data, loading } = useContext(CoinsContext);
+  const limitedCoins = data ? data.slice(0, 4) : null;
   return (
     <>
-     <div className="hero-coins hidden w-full sm:grid grid-cols-2 md:grid-cols-4 justify-items-center gap-10 mt-4 xl:flex xl:justify-center xl:gap-28">
-            <Link
-              to={""}
-              className="flex flex-col justify-center items-center gap-2"
-            >
-              <img
-                className="h-24"
-                src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
-                alt=""
-              />
-              <div className="text-xl">
-                <span>Bitcoin</span>{" "}
-                <span className="text-red-600">-5.58%</span>
-              </div>
-              <span className="text-2xl tracking-wider">$5448.00</span>
-            </Link>
-            <Link
-              to={""}
-              className="flex flex-col justify-center items-center gap-2"
-            >
-              <img
-                className="h-24"
-                src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
-                alt=""
-              />
-              <div className="text-xl">
-                <span>Bitcoin</span> <span>-5.58%</span>
-              </div>
-              <span className="text-2xl tracking-wider">$5448.00</span>
-            </Link>
-            <Link
-              to={""}
-              className="flex flex-col justify-center items-center gap-2"
-            >
-              <img
-                className="h-24"
-                src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
-                alt=""
-              />
-              <div className="text-xl">
-                <span>Bitcoin</span> <span>-5.58%</span>
-              </div>
-              <span className="text-2xl tracking-wider">$5448.00</span>
-            </Link>
-            <Link
-              to={""}
-              className="flex flex-col justify-center items-center gap-2"
-            >
-              <img
-                className="h-24"
-                src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
-                alt=""
-              />
-              <div className="text-xl">
-                <span>Bitcoin</span> <span>-5.58%</span>
-              </div>
-              <span className="text-2xl tracking-wider">$5448.00</span>
-            </Link>
-          </div>
+      {!loading && (
+        <div className="hero-coins hidden w-full sm:grid grid-cols-2 md:grid-cols-4 justify-items-center gap-10 mt-4 xl:flex xl:justify-center xl:gap-28">
+          {limitedCoins.map((item) => {
+            return (
+              <Link
+                key={item.id}
+                to={`/coin/${item.name.toLowerCase()}`}
+                className="flex flex-col justify-center items-center gap-2"
+              >
+                <img className="h-24" src={item.image} alt={item.id} />
+                <div className="text-xl">
+                  <span>{item.name}</span>{" "}
+                  <span className="text-red-600">
+                    {item.price_change_percentage_24h}%
+                  </span>
+                </div>
+                <span className="text-2xl tracking-wider">
+                  ${item.current_price}
+                </span>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
